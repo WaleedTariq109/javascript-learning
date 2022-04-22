9a83014e35e0d5b2aeb0801909ff1c9778989c12
Waleed Tariq <waleedtariq109@outlook.com>
2022-04-23 03:17:09 +0500

    Arrays fundamentals

--- script.js
+++ script.js
@@ -2,4 +2,30 @@
 
 // Arrays
 
+// Method #1
+// Literal Syntax
 const companines = ['Google', 'Apple', 'Microsoft', 'Amazon'];
+
+// Method #2
+// Constructor Function
+const persons = new Array('Waleed', 'Usama', 'Muzammil', 'Samad');
+
+// Retriving value/element from array
+// Syntax: companines[index]
+
+// console.log(companines[0]);
+
+// console.log(companines);
+// console.log(persons);
+
+// Excercise
+const years = [2000, 1992, 2015];
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])];
+
+// console.log(ages);
+
+function calcAge(birthYear) {
+  return 2022 - birthYear;
+}
+
