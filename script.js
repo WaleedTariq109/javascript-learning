897b8e20b477a1b5bff6863bb60027791a0d5b5d
Waleed Tariq <waleedtariq109@outlook.com>
2022-04-25 06:57:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -51,20 +51,49 @@ const foods = new Object({
   food2: 'Butter Chicken',
 });
 
-console.log(waleed);
-console.log(foods);
+// console.log(waleed);
+// console.log(foods);
 
 // Retrive Value from Objects
 
 // Retrive value with dot notation
-console.log(waleed.firstName);
+// console.log(waleed.firstName);
 
 // Retrive value with bracket notation
-console.log(waleed['firstName']);
+// console.log(waleed['firstName']);
 
 // With bracket notation we can also retrive value with some expression
 
 const key = 'Name';
 
-console.log(waleed['first' + key]);
-console.log(waleed['last' + key]);
+// console.log(waleed['first' + key]);
+// console.log(waleed['last' + key]);
+
+////////////////////////////////////////////////////
+
+// Data structures and modern operators
+
+// Array Destructuring
+
+const resturant = {
+  name: 'BBQ Tonight',
+  location: 'Near Askari park',
+  categories: ['fast food', 'Pakistani', 'Chineese', 'Vegetarian'],
+  starterMenu: ['Mint Margarita', 'Fruit Triffel', 'Garlic Bread'],
+  mainMenu: ['Beaf Steak', 'Chicken Karahi', 'Egg fried rice'],
+};
+
+//
+
+const arr = [1, 2, 3];
+
+// Retriving Element without Array Destructuring
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// Retriving Element with Array Destructuring
+const [x, y, z] = arr;
+
+console.log(a, b, c);
+console.log(x, y, z);
