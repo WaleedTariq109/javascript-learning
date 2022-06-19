c9044a9b28ef2cbd75102367410380db79a5a016
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-20 03:20:22 +0500

    Update script.js

--- data-structures-and-operators/script.js
+++ data-structures-and-operators/script.js
@@ -63,6 +63,18 @@ console.log(menu);
 
 // Rest Pattern and Parameter
 
-// Rest
+// Rest Pattern
 const [a, b, ...others] = [1, 2, 3, 4, 5, 6];
 console.log(a, b, others);
+
+// Rest Parameter
+
+function add(...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+}
+
+add(2, 3, 4, 5, 6, 7);
