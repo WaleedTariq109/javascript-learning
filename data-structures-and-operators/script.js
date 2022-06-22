7249939ffae6b905bbc8160ba0ffdb6803db2680
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-23 04:35:41 +0500

    Explore short circuiting

--- data-structures-and-operators/script.js
+++ data-structures-and-operators/script.js
@@ -32,49 +32,77 @@ const restaurant = {
   },
 };
 
-// Array Destructuring
+// // Array Destructuring
 
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
-// const [a, b, c, d] = arr;
-// console.log(a, b, c, d);
+// // const [a, b, c, d] = arr;
+// // console.log(a, b, c, d);
 
-const [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// const [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
-const [starterItem, mainItem] = restaurant.order(1, 2);
+// const [starterItem, mainItem] = restaurant.order(1, 2);
 
-console.log(starterItem, mainItem);
+// console.log(starterItem, mainItem);
 
-// Destructuring Nested Array
+// // Destructuring Nested Array
 
-const nested = [1, 2, [3, 4]];
-const [x, , [y, z]] = nested;
+// const nested = [1, 2, [3, 4]];
+// const [x, , [y, z]] = nested;
 
-console.log(x, y, z);
+// console.log(x, y, z);
 
-// Spread Operator
+// // Spread Operator
 
-const copyMenu = [...restaurant.mainMenu]; // Shallow Copy of an Array
-const menu = [...restaurant.mainMenu, ...restaurant.starterMenu]; // Merge two Arrays
+// const copyMenu = [...restaurant.mainMenu]; // Shallow Copy of an Array
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu]; // Merge two Arrays
 
-console.log(copyMenu);
-console.log(menu);
+// console.log(copyMenu);
+// console.log(menu);
 
-// Rest Pattern and Parameter
+// // Rest Pattern and Parameter
 
-// Rest Pattern
-const [a, b, ...others] = [1, 2, 3, 4, 5, 6];
-console.log(a, b, others);
+// // Rest Pattern
+// const [a, b, ...others] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b, others);
 
-// Rest Parameter
+// // Rest Parameter
 
-function add(...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log(sum);
+// function add(...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// }
+
+// add(2, 3, 4, 5, 6, 7);
+
+// Short Circuiting
+// numGuest dosen't exist in resturant object
+// Without short circuiting
+console.log('---- OR ----');
+
+restaurant.numGuest = 2;
+const guest1 = restaurant.numGuest ? restaurant.numGuest : 10;
+console.log(guest1);
+
+// With short circuiting
+const guest2 = restaurant.numGuest || 20;
+console.log(guest2);
+
+console.log('---- AND ----');
+
+console.log(0 && 10);
+console.log('' && 'Waleed');
+console.log(10 && 'waleed');
+console.log('Waleed' && 10 && null && 'hello');
+
+// Without short circuiting
+if (restaurant.order) {
+  console.log(restaurant.order(2, 1));
 }
 
-add(2, 3, 4, 5, 6, 7);
+// With short circuiting
+console.log(restaurant.order && restaurant.order(2, 1));
