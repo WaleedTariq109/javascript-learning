635cede6c106457e1f2a8963d160194805d95bba
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-29 04:35:19 +0500

    Explore func with default param

--- functions/script.js
+++ functions/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Function with default parameter
+/**
+ * arr [
+ *  { flightNum: PIA009, totalPassengers: 212, price: 12000 }
+ * ]
+ */
+
+const bookings = [];
+
+const createBooking = (flightNum, totalPassengers, price = 10000) => {
+  const booking = {
+    flightNum,
+    totalPassengers,
+    price,
+  };
+  console.log(booking);
+
+  bookings.push(booking);
+};
+
+createBooking('PIA109', 212, 12000);
+createBooking('PIA109', 212);
