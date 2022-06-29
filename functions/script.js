0aa1a6f642bf962c414baf6757edb6557501f67d
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-29 05:17:55 +0500

    Update script.js

--- functions/script.js
+++ functions/script.js
@@ -20,5 +20,29 @@ const createBooking = (flightNum, totalPassengers, price = 10000) => {
   bookings.push(booking);
 };
 
-createBooking('PIA109', 212, 12000);
-createBooking('PIA109', 212);
+// createBooking('PIA109', 212, 12000);
+// createBooking('PIA109', 212);
+
+// Passing Arguments (by value vs by reference)
+
+const flight = 'PIA109';
+const passenger = {
+  name: 'Waleed Tariq',
+  passport: 934937493798,
+};
+
+const checkIn = (flight, person) => {
+  flight = 'EMI109';
+  person.name = `Mr, ${person.name}`;
+
+  if (person.passport === 934937493798) {
+    alert('Check In');
+  } else {
+    alert('Wrong Passport Number');
+  }
+};
+
+checkIn(flight, passenger);
+
+console.log(flight);
+console.log(passenger);
