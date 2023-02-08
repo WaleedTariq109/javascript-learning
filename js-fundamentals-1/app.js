c8bf50d89f0d7ba77d3d7bbe904826c2dbd0e5c0
Waleed Tariq <76990392+waleedtariq109@users.noreply.github.com>
2023-02-09 03:43:34 +0500

    values and variables

---
+++ js-fundamentals-1/app.js
@@ -0,0 +1,15 @@
+// Values and Variables
+
+/**
+ * Value in javascript is just a piece of data it's also a smallest unit of data.
+
+ * 'waleed' is a value
+ * 23 is a value
+
+ */
+
+console.log("waleed");
+console.log(23);
+
+const firstName = "waleed";
+console.log(firstName);
